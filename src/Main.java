697afa3a3f659acb6544326cfb0ba9a21ce349f5
Swapna Kotha <kothaswapna03@gmail.com>
2022-05-13 11:03:31 -0700

    Fixed the errors after integrating code

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,74 @@
 import com.sjsu.cmpe.sort.service.impl.QuickSortMultiThread;
 import com.sjsu.cmpe.sort.service.impl.QuickSortSingleThread;
+import com.sjsu.cmpe.sort.service.impl.MergeSortMultiThread;
+import com.sjsu.cmpe.sort.service.impl.MergeSortSingleThread;
 
 public class Main {
     public static void main(String[] args) {
 
         Comparable intArray[] = {1,4,6,25,74,7,4};
         Comparable intArrayCopy[] = intArray.clone();
+        MergeSortSingleThread ms_singleThread = new MergeSortSingleThread(intArray);
+        ms_singleThread.sort();
+        System.out.println("Execution time for MergeSort for Integer array size:"+intArray.length+" with one thread: " + ms_singleThread.getExecutionTime());
+
+        MergeSortMultiThread ms_multiThread = new MergeSortMultiThread(intArrayCopy);
+        ms_multiThread.sort();
+        System.out.println("Execution time for MergeSort for Integer array size:"+intArray.length+" with multi thread: " + ms_multiThread.getExecutionTime());
+
+        intArray = new Comparable[]{1,4,6,25,74,7,4};
+        intArrayCopy = intArray.clone();
         QuickSortSingleThread qs_singleThread = new QuickSortSingleThread(intArray);
         qs_singleThread.sort();
-        System.out.println("Execution time for QuickSort with one thread: " + qs_singleThread.getExecutionTime());
+        System.out.println("Execution time for QuickSort for Integer array size:"+intArray.length+" with one thread: " + qs_singleThread.getExecutionTime());
 
         QuickSortMultiThread qs_multiThread = new QuickSortMultiThread(intArrayCopy);
         qs_multiThread.sort();
-        System.out.println("Execution time for QuickSort with multi thread: " + qs_multiThread.getExecutionTime());
+        System.out.println("Execution time for QuickSort for Integer array size:"+intArray.length+" with multi thread: " + qs_multiThread.getExecutionTime());
 
 
         intArray = getRandomIntArray(50000);
         intArrayCopy = intArray.clone();
+        ms_singleThread = new MergeSortSingleThread(intArray);
+        ms_singleThread.sort();
+        System.out.println("Execution time for MergeSort for Integer array size:"+intArray.length+" with one thread: " + ms_singleThread.getExecutionTime());
+
+        ms_multiThread = new MergeSortMultiThread(intArrayCopy);
+        ms_multiThread.sort();
+        System.out.println("Execution time for MergeSort for Integer array size:"+intArray.length+" with multi thread: " + ms_multiThread.getExecutionTime());
+
+        intArray = getRandomIntArray(50000);
+        intArrayCopy = intArray.clone();
+        
         qs_singleThread = new QuickSortSingleThread(intArray);
         qs_singleThread.sort();
-        System.out.println("Execution time for QuickSort with one thread: " + qs_singleThread.getExecutionTime());
+        System.out.println("Execution time for QuickSort for Integer array size:"+intArray.length+" with one thread: " + qs_singleThread.getExecutionTime());
 
         qs_multiThread = new QuickSortMultiThread(intArrayCopy);
         qs_multiThread.sort();
-        System.out.println("Execution time for QuickSort with multi thread: " + qs_multiThread.getExecutionTime());
+        System.out.println("Execution time for QuickSort for Integer array size:"+intArray.length+" with multi thread: " + qs_multiThread.getExecutionTime());
 
         String[] stringArray = getAlphaNumericString(5000);
         String[] stringArrayCopy = stringArray.clone();
+
+        ms_singleThread = new MergeSortSingleThread(stringArray);
+        ms_singleThread.sort();
+        System.out.println("Execution time for MergeSort for String array size:"+stringArray.length+" with one thread: " + ms_singleThread.getExecutionTime());
+
+        ms_multiThread = new MergeSortMultiThread(stringArrayCopy);
+        ms_multiThread.sort();
+        System.out.println("Execution time for MergeSort for String array size:"+stringArray.length+" with multi thread: " + ms_multiThread.getExecutionTime());
+
+        stringArray = getAlphaNumericString(50000);
+        stringArrayCopy = stringArray.clone();
+
         qs_singleThread = new QuickSortSingleThread(stringArray);
         qs_singleThread.sort();
-        System.out.println("Execution time for QuickSort with one thread: " + qs_singleThread.getExecutionTime());
+        System.out.println("Execution time for QuickSort for String array size:"+stringArray.length+" with one thread: " + qs_singleThread.getExecutionTime());
+        
         qs_multiThread = new QuickSortMultiThread(stringArrayCopy);
         qs_multiThread.sort();
-        System.out.println("Execution time for QuickSort with multi thread: " + qs_multiThread.getExecutionTime());
+        System.out.println("Execution time for QuickSort for String array size:"+stringArray.length+" with multi thread: " + qs_multiThread.getExecutionTime());
     }
 
     static Comparable[] getRandomIntArray(int length) {
--- src/com/sjsu/cmpe/sort/service/impl/MergeSortMultiThread.java
+++ src/com/sjsu/cmpe/sort/service/impl/MergeSortMultiThread.java
@@ -11,8 +11,9 @@ public class MergeSortMultiThread implements Sort {
     long executionTime = 0;
     Utility utility;
 
-    MergeSortMultiThread(Comparable[] arr) {
+    public MergeSortMultiThread(Comparable[] arr) {
         this.arr = arr;
+        utility = new Utility();
     }
 
     public void sort() {
@@ -28,9 +29,38 @@ public class MergeSortMultiThread implements Sort {
             T2.fork();
             T1.join();
             T2.join();
+            merge(arr, 0, half, end);
         }
         long endTime = System.currentTimeMillis();
         executionTime = endTime - startTime;
+    }
+
+    public void merge(Comparable[] arr, int begin, int half, int end) {
+        int begin2 = half + 1, i = begin;
+        Comparable[] temp = Arrays.copyOf(arr, arr.length);
+
+        while ((begin <= half) && (begin2 <= end)) {
+            if (temp[begin].compareTo(temp[begin2]) < 0) {
+                arr[i] = temp[begin];
+                begin++;
+            } else {
+                arr[i] = temp[begin2];
+                begin2++;
+            }
+            i++;
+        }
+
+        while (begin <= half) {
+            arr[i] = temp[begin];
+            i++;
+            begin++;
+        }
+
+        while (begin2 <= end) {
+            arr[i] = temp[begin2];
+            i++;
+            begin2++;
+        }
     }
 
     class MergeSortTask extends RecursiveAction {
@@ -55,34 +85,6 @@ public class MergeSortMultiThread implements Sort {
                 mergeSort(arr, begin, half);
                 mergeSort(arr, half + 1, end);
                 merge(arr, begin, half, end);
-            }
-        }
-
-        public void merge(Comparable[] arr, int begin, int half, int end) {
-            int begin2 = half + 1, i = begin;
-            Comparable[] temp = Arrays.copyOf(arr, arr.length);
-
-            while ((begin <= half) && (begin2 <= end)) {
-                if (temp[begin].compareTo(temp[begin2]) < 0) {
-                    arr[i] = temp[begin];
-                    begin++;
-                } else {
-                    arr[i] = temp[begin2];
-                    begin2++;
-                }
-                i++;
-            }
-
-            while (begin <= half) {
-                arr[i] = temp[begin];
-                i++;
-                begin++;
-            }
-
-            while (begin2 <= end) {
-                arr[i] = temp[begin2];
-                i++;
-                begin2++;
             }
         }
     }
